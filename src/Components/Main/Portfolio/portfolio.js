f68e9874a0026d489c003b70c6ef36b3eca37c3e
Armando Mossuto <armandomossuto@gmail.com>
2018-11-04 14:02:25 +0000

    Final updateon React Router

--- src/Components/Main/Portfolio/portfolio.js
+++ src/Components/Main/Portfolio/portfolio.js
@@ -4,6 +4,14 @@ const Portfolio = () =>
   <div className="projectspage"> 
     <h2>Portfolio</h2>
     <div className="projectsbox">
+    <div className="projectrow">
+          <div className="projectelement">
+            <a href="/Job-Search-App-Source/" target="_blank" rel="noopener noreferrer">
+            <img src="job-search-app_photo.png" />
+            </a>
+          </div>
+        <p>Job Search App</p>
+      </div>
       <div className="projectrow">
           <div className="projectelement">
             <a href="/Movie-Recommendation-App/" target="_blank" rel="noopener noreferrer">
