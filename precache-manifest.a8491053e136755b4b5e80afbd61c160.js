9ec217972d4a21894989d176d4e2155de2832def
armandomossuto <armandomossuto@gmail.com>
2018-11-09 21:47:56 +0000

    Updates

---
+++ precache-manifest.a8491053e136755b4b5e80afbd61c160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b122a09584dcdaa9a36",
+    "url": "/static/js/main.1b122a09.chunk.js"
+  },
+  {
+    "revision": "6e47fc670e49a3023268",
+    "url": "/static/js/1.6e47fc67.chunk.js"
+  },
+  {
+    "revision": "1b122a09584dcdaa9a36",
+    "url": "/static/css/main.d4082b4f.chunk.css"
+  },
+  {
+    "revision": "4fc0974b03a1f39273d01af455678b73",
+    "url": "/index.html"
+  }
+];
